b539b87b1d5cba98dd0410523ab3e672d6695494
Becky <becky@vandestreek.net>
2018-09-21 03:59:53 +0200

    Fill/empty Star in App Bar functions added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ function switchChannel(channelName) {
     document.getElementById('location-app-bar-right').innerHTML = '<strong>upgrading.never.helps</strong>';
     document.getElementById('location-app-bar-right').href = "https://map.what3words.com/upgrading.never.helps";
 }
+function emptyStar() {
+    $("#app-bar-star").attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    $("#app-bar-star").attr('alt', 'alt="not starred');
+}
+
+function fillStar() {
+    $("#app-bar-star").attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    $("#app-bar-star").attr('alt', 'alt="starred');
+}
