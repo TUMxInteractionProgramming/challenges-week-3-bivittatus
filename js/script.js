e93c9d97e2206a9df448a0daea670f6666f9d9cf
Becky <becky@vandestreek.net>
2018-09-21 02:01:54 +0200

    JS Location Change Added

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ console.log('app is alive')
 function switchChannel(channelName) {
     console.log('Tuning into channel ' + channelName)
     document.getElementById('current-channel').innerHTML = channelName;
+    document.getElementById('location-app-bar-right').innerHTML = '<strong>upgrading.never.helps</strong>';
+    document.getElementById('location-app-bar-right').href = "https://map.what3words.com/upgrading.never.helps";
 }
